338cf3e4f0f2c81a87b60fd5880c1a9356980fbd
L0live <yann.olive@outlook.com>
2025-08-11 15:00:19 +0200

    add Login form to Home

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -9,6 +9,33 @@ export default function Home(): HTMLElement {
   title.className = 'text-[10rem] font-extrabold text-green-400 drop-shadow-[0_0_10px_#00ff00]';
   container.appendChild(title);
 
+  // Conteneur du login
+  const loginWrapper = document.createElement('div');
+  loginWrapper.className = 'flex flex-col justify-center items-center gap-12 p-16';
+
+  const loginTitle = document.createElement('h2');
+  loginTitle.textContent = 'Login';
+  loginTitle.className = 'text-4xl font-bold text-white';
+  loginWrapper.appendChild(loginTitle);
+
+  const loginNicknameInput = document.createElement('input');
+  loginNicknameInput.placeholder = 'Enter your nickname';
+  loginNicknameInput.className = 'text-black p-2 rounded-md w-[220px]';
+  loginWrapper.appendChild(loginNicknameInput);
+
+  const loginPasswordInput = document.createElement('input');
+  loginPasswordInput.placeholder = 'Enter your password';
+  loginPasswordInput.type = 'password';
+  loginPasswordInput.className = 'text-black p-2 rounded-md w-[220px]';
+  loginWrapper.appendChild(loginPasswordInput);
+
+  const loginButton = document.createElement('button');
+  loginButton.textContent = 'Login';
+  loginButton.className = 'bg-[#646cff] text-white p-2 rounded-md w-[80px]';
+  loginWrapper.appendChild(loginButton);
+
+  container.appendChild(loginWrapper);
+
   // Conteneur des boutons
   const buttonsWrapper = document.createElement('div');
   buttonsWrapper.className = 'flex flex-wrap justify-center gap-8';
@@ -17,9 +44,9 @@ export default function Home(): HTMLElement {
   const createButton = (label: string, route: string): HTMLElement => {
     const button = document.createElement('button');
     button.className = [
-      'w-[160px] h-[160px]', // fixed size
+      'w-[320px] h-[320px]', // fixed size
       'bg-[#646cff] rounded-xl',
-      'hover:w-[164px] hover:h-[164px] hover:bg-[#535bf2]',
+      'hover:w-[330px] hover:h-[330px] hover:bg-[#535bf2]',
       'hover:drop-shadow-[0_0_10px_#535bf2]',
       'transition-all duration-300', // transition-all for smoother effect
       'flex items-center justify-center' // center text vertically and horizontally
